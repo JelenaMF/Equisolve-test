54f42204b869008aaf92953c22d75ab351fae796
Jelena Feliciano <68608110+JelenaMF@users.noreply.github.com>
2021-07-19 02:23:16 -0400

    Add files via upload

---
+++ test/index.php
@@ -0,0 +1,36 @@
+<?php 
+
+//MySQLi
+$conn = mysqli_connect('localhost', 'Equisolve', '[-9j.2rbA6CMCOJp', 'equisolve team');
+//check connection
+if(!$conn){
+echo 'connection error: '. mysqli_connect_error();
+}
+//write query for all bios
+$sql = 'SELECT  * FROM team_bios';
+//make query & get results
+$results = mysqli_query($conn, $sql);
+
+//fetch the resulting rows as array
+$bios = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
+
+//free result from memory 
+mysqli_free_result($results);
+//close connection
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
+<html>
+    <?php include('templates/header.php'); ?>
+        
+    <?php include('templates/banner.php'); ?>
+
+    <?php include('templates/bios.php'); ?>
+
+   
+
+    <?php include('templates/footer.php'); ?>
+
+</html>
